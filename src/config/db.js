73993c8c32d3c5dce22c3e8aae4650e3d22ea525
Lokesh shinde <lokeshinde500@gmail.com>
2024-10-10 05:36:07 +0530

    fix database issue

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import constant from "./constant.js";
 
 const db = async () => {
   await mongoose
-    .connect(constant.DB_LOCAL)
+    .connect(constant.DB_URL)
     .then(() => {
       console.log("DB connected :)");
     })
